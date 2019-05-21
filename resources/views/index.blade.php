86a380baf93254699604dbd443fab8296f0130c1
EsliUchiwa <yetizer20@gmail.com>
2019-05-21 23:10:07 +0100

    yeah

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -221,7 +221,7 @@
     <div class="row">
       <div class="col-lg-6 col-sm-9 recent-view">
         <h3>A propos</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br><a href="{{ url('/about') }}">Savoir plus</a></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br><a href="{{ url('/about') }}">En savoir plus</a></p>
       
       </div>
       <div class="col-lg-5 col-lg-offset-1 col-sm-3 recommended">
